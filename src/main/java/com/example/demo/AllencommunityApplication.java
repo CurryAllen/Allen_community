0b42704590576ebd2d67f94348787752378d94d9
CurryAllen <swe1709520@xmu.edu.my>
2019-12-01 20:00:27 +0800

    init repo

---
+++ src/main/java/com/example/demo/AllencommunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllencommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllencommunityApplication.class, args);
+	}
+
+}
